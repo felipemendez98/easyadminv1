b52977f32aebc3f79d9b600e80f7881f64aa9439
Felipe Mendez <cfelipecm26@gmail.com>
2021-08-17 18:37:35 -0500

    depurando side bar y añadiendo el router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './components/personas/personas.component';
@@ -10,16 +11,20 @@ import { HomeComponent } from './components/home/home.component';
 import { ProductoComponent } from './components/producto/producto.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { FormComponent } from './components/form/form.component';
-import { IframeComponent } from './components/iframe/iframe.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ProveedoresComponent } from './components/proveedores/proveedores.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent, PersonasComponent, PersonaComponent, HeaderComponent, SidebarComponent, HomeComponent, ProductoComponent, ClientesComponent, FormComponent, IframeComponent, MenuComponent,
+    AppComponent, PersonasComponent, PersonaComponent, HeaderComponent, SidebarComponent, HomeComponent, ProductoComponent, ClientesComponent, FormComponent, MenuComponent, ProveedoresComponent, VentasComponent, OfertasComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
